e5f5cf33e970a51612d9c6307fd6601f75cfc325
agostinoluca <agostino.luca89@gmail.com>
2023-12-02 14:29:57 +0100

    add track, tools and istruction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,55 @@
-console.log('Hello World');
+/* 
+
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+##Tools
+ -const
+ -prompt
+ -fuction
+ -return
+ -document.GetElementById/querySelector
+ -.innerHtml .innerText .textContent
+
+*/ 
+
+// con un prompt chiedo all'utente di inserire una parola
+
+// con una fuction chiedo a javascript di verificare se la parola inserita è palindroma
+
+// stampo in pagina un messaggio che afferma se la parola è o non è palindroma
+
+
+
+
+
+
+/*
+
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+
+##Tools
+ -const
+ -prompt
+ -fuction
+ -math.random
+ -return
+ -document.GetElementById/querySelector
+ -.innerHtml .innerText .textContent
+
+*/
+
+// chiedo all'utente se sceglie pari o dispari
+
+// con un prompt chiedo all'utente di inserire un numero da 1 a 5
+
+// con una funzione generiamo un numero casuale da 1 a 5 per il computer
+
+// con una funzione sommiamo i numeri e verifichiamo se il numero ottenuto è pari o dispari
+
+// stampo in pagina il vincitore
